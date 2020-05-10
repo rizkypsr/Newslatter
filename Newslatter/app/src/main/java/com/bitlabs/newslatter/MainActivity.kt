779cad1abc06ed17610e79f38598781a16c321b5
rizkypsr <rizkypsrr@gmail.com>
2020-05-10 17:29:00 +0700

    Added function to validate empty field name

--- Newslatter/app/src/main/java/com/bitlabs/newslatter/MainActivity.kt
+++ Newslatter/app/src/main/java/com/bitlabs/newslatter/MainActivity.kt
@@ -2,11 +2,22 @@ package com.bitlabs.newslatter
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun validateName(): Boolean {
+        if (edit_name.text.length == 0) {
+            error_name.text = "Name must be filled"
+            return false
+        } else {
+            error_name.text = null
+            return true
+        }
     }
 }
