d3c3a89be898708e5c90010f05323d8cae3de4ec
rizkypsr <rizkypsrr@gmail.com>
2020-05-10 17:49:43 +0700

    Added click listener to button and Called func to validate form

--- Newslatter/app/src/main/java/com/bitlabs/newslatter/MainActivity.kt
+++ Newslatter/app/src/main/java/com/bitlabs/newslatter/MainActivity.kt
@@ -11,6 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_submit.setOnClickListener {
+            validateForm()
+        }
     }
 
     fun validateForm() {
