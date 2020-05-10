1c257f15d85016dc32ed634bdf8c6277f412af72
rizkypsr <rizkypsrr@gmail.com>
2020-05-10 17:35:20 +0700

    Added func to validate email

--- Newslatter/app/src/main/java/com/bitlabs/newslatter/MainActivity.kt
+++ Newslatter/app/src/main/java/com/bitlabs/newslatter/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.bitlabs.newslatter
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,6 +18,19 @@ class MainActivity : AppCompatActivity() {
             return false
         } else {
             error_name.text = null
+            return true
+        }
+    }
+
+    fun validateEmail(): Boolean {
+        if (edit_email.text.isEmpty()) {
+            error_email.text = "Email must be filled"
+            return false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(edit_email.text).matches()) {
+            error_email.text = "Must Match Email Pattern"
+            return false
+        } else {
+            error_email.text = null
             return true
         }
     }
