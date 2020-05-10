d389c9da2a4e0a6c7a2d95f0506c66357a7e1d78
rizkypsr <rizkypsrr@gmail.com>
2020-05-10 20:37:55 +0700

    Create View Holder Class

---
+++ Newslatter/app/src/main/java/com/bitlabs/newsletter/viewholder/ItemViewHolder.kt
@@ -0,0 +1,11 @@
+package com.bitlabs.newsletter.viewholder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    val title = itemView.title
+    val body = itemView.body
+}
