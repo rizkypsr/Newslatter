3286e44f40673360d2a253bf442e820f2be3dded
rizkypsr <rizkypsrr@gmail.com>
2020-05-10 21:02:14 +0700

    Added Image Profile Using Glide

--- Newslatter/app/src/main/java/com/bitlabs/newsletter/activities/ProfileActivity.kt
+++ Newslatter/app/src/main/java/com/bitlabs/newsletter/activities/ProfileActivity.kt
@@ -2,8 +2,10 @@ package com.bitlabs.newsletter.activities
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bitlabs.newsletter.R
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
@@ -20,5 +22,10 @@ class ProfileActivity : AppCompatActivity() {
         sharedPref = this.getSharedPreferences(PREF_NAME, PRIVATE_MODE)
         user_profile_name.text = sharedPref?.getString("user-name", "")
         user_profile_email.text = sharedPref?.getString("user-email", "")
+
+        val image: ImageView = findViewById(R.id.img_profile)
+        Glide.with(this)
+            .load("https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_1280.png")
+            .into(image)
     }
 }
