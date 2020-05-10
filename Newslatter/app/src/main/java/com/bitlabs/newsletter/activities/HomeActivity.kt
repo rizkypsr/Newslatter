dbf20ac509f26814cc466f5d6cf733497c594e63
rizkypsr <rizkypsrr@gmail.com>
2020-05-10 20:47:31 +0700

    Create Recycler View in HomeActivity

--- Newslatter/app/src/main/java/com/bitlabs/newsletter/activities/HomeActivity.kt
+++ Newslatter/app/src/main/java/com/bitlabs/newsletter/activities/HomeActivity.kt
@@ -5,14 +5,34 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bitlabs.newsletter.MainActivity
 import com.bitlabs.newsletter.R
+import com.bitlabs.newsletter.adapter.PostAdapter
+import com.bitlabs.newsletter.model.Post
 
 class HomeActivity : AppCompatActivity() {
+
+    private val listPost = ArrayList<Post>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    fun initData() {
+        listPost.add(Post("Android 10", "Android meluncurkan 0S terbaru", "20 Jun 2019"))
+        listPost.add(Post("Kebocoran Gas", "Kebocoran gas di lampung", "10 May 2019"))
+    }
+
+    fun iniRecyclerView() {
+        val rView = findViewById<RecyclerView>(R.id.recycler_view)
+        val adapter = PostAdapter(listPost, this)
+        rView.layoutManager = LinearLayoutManager(this)
+        rView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+        rView.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
