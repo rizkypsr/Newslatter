bb5c770c2ccb0ac605d6efd1567d973cdac4224c
rizkypsr <rizkypsrr@gmail.com>
2020-05-12 18:25:52 +0700

    Added button delete listener & call func to delete news from db

--- Newslatter/app/src/main/java/com/bitlabs/newsletter/adapter/PostAdapter.kt
+++ Newslatter/app/src/main/java/com/bitlabs/newsletter/adapter/PostAdapter.kt
@@ -1,12 +1,15 @@
 package com.bitlabs.newsletter.adapter
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bitlabs.newsletter.R
 import com.bitlabs.newsletter.activities.CreateNewsActivity
+import com.bitlabs.newsletter.helper.NewsletterDBHelper
 import com.bitlabs.newsletter.model.News
 import com.bitlabs.newsletter.viewholder.ItemViewHolder
 
@@ -27,11 +30,18 @@ class PostAdapter(val items: ArrayList<News>, val context: Context) :
         holder.date.text = items[position].date
         holder.btnUpdate.setOnClickListener {
             val intent = Intent(context, CreateNewsActivity::class.java)
-            intent.putExtra("id_extra", items[position].id)
-            intent.putExtra("title_extra", items[position].title)
-            intent.putExtra("body_extra", items[position].body)
-            intent.putExtra("date_extra", items[position].date)
+            intent.putExtra("id-extra", items[position].id)
+            intent.putExtra("title-extra", items[position].title)
+            intent.putExtra("body-extra", items[position].body)
+            intent.putExtra("date-extra", items[position].date)
             context.startActivity(intent)
+        }
+        holder.btnDelete.setOnClickListener {
+            val db = NewsletterDBHelper(context)
+            db.deleteNews(items[position].id)
+            (context as Activity).finish()
+            context.startActivity(context.intent)
+            Toast.makeText(context, "Success Delete Data", Toast.LENGTH_SHORT).show()
         }
     }
 }
