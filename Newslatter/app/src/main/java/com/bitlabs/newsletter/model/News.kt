4b8cfcefc5910cf310085db0118511c2d144493f
rizkypsr <rizkypsrr@gmail.com>
2020-05-12 18:26:32 +0700

    fix class

---
+++ Newslatter/app/src/main/java/com/bitlabs/newsletter/model/News.kt
@@ -0,0 +1,12 @@
+package com.bitlabs.newsletter.model
+
+class News(
+    title: String,
+    body: String,
+    date: String
+) {
+    var id: Int = 0
+    var title: String = title
+    var body: String = body
+    var date: String = date
+}
