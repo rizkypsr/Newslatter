eb909b85f5fc2d23ca901638e0cc75b53f677478
rizkypsr <rizkypsrr@gmail.com>
2020-05-12 18:24:27 +0700

    set content view, fix if expression

--- Newslatter/app/src/main/java/com/bitlabs/newsletter/activities/CreateNewsActivity.kt
+++ Newslatter/app/src/main/java/com/bitlabs/newsletter/activities/CreateNewsActivity.kt
@@ -2,24 +2,25 @@ package com.bitlabs.newsletter.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bitlabs.newsletter.R
 import com.bitlabs.newsletter.helper.NewsletterDBHelper
-import com.bitlabs.newsletter.model.Post
+import com.bitlabs.newsletter.model.News
 import kotlinx.android.synthetic.main.activity_create_news.*
 
 class CreateNewsActivity : AppCompatActivity() {
 
     private val db = NewsletterDBHelper(this)
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_news)
 
-        if (intent.getStringExtra("title-extra") == "") {
+        if (intent.getIntExtra("id-extra", 0) == 0) {
             btn_insert.setOnClickListener {
                 insertNews()
-                Toast.makeText(this, "Success Insert Data", Toast.LENGTH_SHORT)
+                Toast.makeText(this, "Success Insert Data", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this@CreateNewsActivity, HomeActivity::class.java)
                 startActivity(intent)
             }
@@ -30,18 +31,15 @@ class CreateNewsActivity : AppCompatActivity() {
             input_date.setText(intent.getStringExtra("date-extra"))
             btn_insert.setOnClickListener {
                 updateNews()
-                Toast.makeText(this, "Success Update Data", Toast.LENGTH_SHORT)
+                Toast.makeText(this, "Success Update Data", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this@CreateNewsActivity, HomeActivity::class.java)
                 startActivity(intent)
             }
         }
-
     }
 
     private fun insertNews() {
-        val id = intent.getIntExtra("id-extra", 0)
-        val news = Post(
-            id,
+        val news = News(
             input_title.text.toString(),
             input_body.text.toString(),
             input_date.text.toString()
@@ -51,8 +49,7 @@ class CreateNewsActivity : AppCompatActivity() {
 
     private fun updateNews() {
         val id = intent.getIntExtra("id-extra", 0)
-        val news = Post(
-            id,
+        val news = News(
             input_title.text.toString(),
             input_body.text.toString(),
             input_date.text.toString()
