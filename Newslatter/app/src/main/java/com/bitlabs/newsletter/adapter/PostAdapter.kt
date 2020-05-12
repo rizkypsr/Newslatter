937940d20068b79758e91c3ea79e61a1471b8d39
rizkypsr <rizkypsrr@gmail.com>
2020-05-12 18:02:07 +0700

    change name body

--- Newslatter/app/src/main/java/com/bitlabs/newsletter/adapter/PostAdapter.kt
+++ Newslatter/app/src/main/java/com/bitlabs/newsletter/adapter/PostAdapter.kt
@@ -7,10 +7,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bitlabs.newsletter.R
 import com.bitlabs.newsletter.activities.CreateNewsActivity
-import com.bitlabs.newsletter.model.Post
+import com.bitlabs.newsletter.model.News
 import com.bitlabs.newsletter.viewholder.ItemViewHolder
 
-class PostAdapter(val items: ArrayList<Post>, val context: Context) :
+class PostAdapter(val items: ArrayList<News>, val context: Context) :
     RecyclerView.Adapter<ItemViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -23,14 +23,14 @@ class PostAdapter(val items: ArrayList<Post>, val context: Context) :
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         holder.title.text = items[position].title
-        holder.body.text = items[position].detail
+        holder.body.text = items[position].body
         holder.date.text = items[position].date
         holder.btnUpdate.setOnClickListener {
             val intent = Intent(context, CreateNewsActivity::class.java)
-            intent.putExtra("id_extra", items.get(position).id)
-            intent.putExtra("title_extra", items.get(position).title)
-            intent.putExtra("body_extra", items.get(position).detail)
-            intent.putExtra("date_extra", items.get(position).date)
+            intent.putExtra("id_extra", items[position].id)
+            intent.putExtra("title_extra", items[position].title)
+            intent.putExtra("body_extra", items[position].body)
+            intent.putExtra("date_extra", items[position].date)
             context.startActivity(intent)
         }
     }
