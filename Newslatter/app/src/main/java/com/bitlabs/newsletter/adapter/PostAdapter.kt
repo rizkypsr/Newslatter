9dcf2b7da3d702f9e43b6df6a2a83fc69aac32dd
rizkypsr <rizkypsrr@gmail.com>
2020-05-12 17:28:44 +0700

    Added listener btnUpdate and Create intent

--- Newslatter/app/src/main/java/com/bitlabs/newsletter/adapter/PostAdapter.kt
+++ Newslatter/app/src/main/java/com/bitlabs/newsletter/adapter/PostAdapter.kt
@@ -1,10 +1,12 @@
 package com.bitlabs.newsletter.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bitlabs.newsletter.R
+import com.bitlabs.newsletter.activities.CreateNewsActivity
 import com.bitlabs.newsletter.model.Post
 import com.bitlabs.newsletter.viewholder.ItemViewHolder
 
@@ -23,5 +25,13 @@ class PostAdapter(val items: ArrayList<Post>, val context: Context) :
         holder.title.text = items[position].title
         holder.body.text = items[position].detail
         holder.date.text = items[position].date
+        holder.btnUpdate.setOnClickListener {
+            val intent = Intent(context, CreateNewsActivity::class.java)
+            intent.putExtra("id_extra", items.get(position).id)
+            intent.putExtra("title_extra", items.get(position).title)
+            intent.putExtra("body_extra", items.get(position).detail)
+            intent.putExtra("date_extra", items.get(position).date)
+            context.startActivity(intent)
+        }
     }
 }
